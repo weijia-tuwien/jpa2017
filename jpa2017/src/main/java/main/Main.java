acc625a19834c270d53b78c3069fa15389a330e6
wei jia <jah_wei@hotmail.com>
2017-03-01 18:16:13 +0100

    setup jpa

--- jpa2017/src/main/java/main/Main.java
+++ jpa2017/src/main/java/main/Main.java
@@ -1,11 +1,29 @@
 package main;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.apache.log4j.Logger;
+
+import model.User;
 
 public class Main {
 	public static void main(String[] args) {
 		Logger logger = Logger.getRootLogger();
 		logger.debug("hallo world");
+
+		User u1 = new User("jordan");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-data");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(u1);
+		em.getTransaction().commit();
+		em.close();
+
+		emf.close();
+
 	}
 
 }
---
+++ jpa2017/src/main/java/model/User.java
@@ -0,0 +1,36 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private Long Id;
+
+	private String name;
+
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
