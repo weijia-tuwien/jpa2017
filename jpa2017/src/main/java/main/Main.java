51fe8f52c2f85ec7401fbd94761d812698de0d36
wei jia <jah_wei@hotmail.com>
2017-03-01 10:00:58 +0100

    log4j added

---
+++ jpa2017/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+	public static void main(String[] args) {
+		Logger logger = Logger.getRootLogger();
+		logger.debug("hallo world");
+	}
+
+}
